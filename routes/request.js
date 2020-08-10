9d617bb2aeab335d1290109c5214b4c7403e2b15
Meet Patel <mpatel.patel180796@gmail.com>
2020-08-10 18:00:32 -0400

    fetched moviedata from the server

---
+++ routes/request.js
@@ -0,0 +1,15 @@
+const API_KEY = "1f534d1a12ce6db079be8b86e1f4ed79";
+
+const request = {
+  fetchGenres: `/genre/movie/list?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginal: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrerMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+module.exports = request;
--- routes/tmdbAPI.js
+++ routes/tmdbAPI.js
@@ -1,26 +1,61 @@
 const express = require("express");
 const axios = require("axios").default;
-
+const requestURL = require("./request");
 const router = express.Router();
 
-const url =
-  "https://api.themoviedb.org/3/movie/550?api_key=1f534d1a12ce6db079be8b86e1f4ed79";
+const url = "https://api.themoviedb.org/3";
 
-const getMovies = async () => {
+const getMovies = async (fetch) => {
   try {
-    const response = await axios.get(url);
+    const { data } = await axios.get(url + fetch);
+    return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-router.get("/", async (req, res) => {
-  try {
-    const { data } = await axios.get(url);
-    res.send(data);
-  } catch (error) {
-    console.log("error in fetching movies", error);
-  }
+router.get("/fetchGenres", async (req, res) => {
+  const data = await getMovies(requestURL.fetchGenres);
+  res.send(data);
+});
+
+router.get("/fetchTrending", async (req, res) => {
+  const data = await getMovies(requestURL.fetchTrending);
+  res.send(data);
+});
+
+router.get("/fetchNetflixOriginal", async (req, res) => {
+  const data = await getMovies(requestURL.fetchNetflixOriginal);
+  res.send(data);
+});
+
+router.get("/fetchTopRated", async (req, res) => {
+  const data = await getMovies(requestURL.fetchTopRated);
+  res.send(data);
+});
+router.get("/fetchActionMovies", async (req, res) => {
+  const data = await getMovies(requestURL.fetchActionMovies);
+  res.send(data);
+});
+
+router.get("/fetchComedyMovies", async (req, res) => {
+  const data = await getMovies(requestURL.fetchComedyMovies);
+  res.send(data);
+});
+
+router.get("/fetchRomanceMovies", async (req, res) => {
+  const data = await getMovies(requestURL.fetchRomanceMovies);
+  res.send(data);
+});
+
+router.get("/fetchHorrerMovies", async (req, res) => {
+  const data = await getMovies(requestURL.fetchHorrerMovies);
+  res.send(data);
+});
+
+router.get("/fetchDocumentariesMovies", async (req, res) => {
+  const data = await getMovies(requestURL.fetchDocumentariesMovies);
+  res.send(data);
 });
 
 module.exports = router;
